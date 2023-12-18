d44cda8e39c0f51edda6b7944aefc282ed9630fe
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-18 13:23:29 +0600

    add navbar and react router dom

---
+++ src/Components/Shared/NavBar.jsx
@@ -0,0 +1,38 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" className="bg-white text-light">
+                <Container>
+                    <Navbar.Brand href="#home">Admin DashBoard</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Signin</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../Components/Shared/NavBar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar />
+            <Outlet />
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Main;
